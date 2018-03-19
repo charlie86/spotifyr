4fe162a9e467c628e7cf1ecbdd169c0900a6b6bf
charlie86 <chuck@storyblocks.com>
2018-03-19 10:59:06 -0400

    suggestions to get_track

--- R/get_track.R
+++ R/get_track.R
@@ -1,9 +1,10 @@
 #' Get track uri:s from a string search on Spotify
 #'
-#' This function returns a data frame with two lists of track name and track uri
-#' returned from Spotfiys search function
-#' @param artist_name A string with artist name
+#' This function takes a string and returns a data frame with track information
+#' from Spotify's search endpoint
 #' @param track_name A string with track name
+#' @param artist_name Optional. A string with artist name
+#' @param album_name Optional. A string with album name
 #' @param access_token Spotify Web API token. Defaults to spotifyr::get_spotify_access_token()
 #' @keywords track uri string search
 #' @export
@@ -13,27 +14,40 @@
 #' kid_a <- get_track(artist_name = "Radiohead", track_name = "Kid A")
 #' }
 
-get_track <- function(artist_name, track_name, access_token = get_spotify_access_token()) {
-    string_search <- paste(artist_name, track_name)
+get_track <- function(track_name, artist_name = NULL, album_name = NULL, access_token = get_spotify_access_token()) {
+
+    string_search <- track_name
+
+    if (!is.null(artist_name)) {
+        string_search <- paste(string_search, artist_name)
+    }
+
+    if (!is.null(album_name)) {
+        string_search <- paste(string_search, album_name)
+    }
+
     # Search Spotify API for track name
     res <- GET('https://api.spotify.com/v1/search',
                query = list(q = string_search,
                             type = 'track', access_token = access_token)) %>%
         content
 
-    if(length(res$tracks$items) == 0){
-        return(data.frame(track_name = c(NA), track_uri = c(NA)))
-    } else {
+    if (length(res$tracks$items) >= 0) {
+
         res <- res %>% .$tracks %>% .$items
-        # Clean response and combine all returned tracks into a dataframe
-        tracks <- map(seq_len(length(res)), function(x) {
+
+        tracks <- map_df(seq_len(length(res)), function(x) {
             list(
                 track_name = res[[x]]$name,
-                track_uri = gsub('spotify:track:', '', res[[x]]$uri) # remove meta info from the uri string
+                track_uri = gsub('spotify:track:', '', res[[x]]$uri),
+                artist_name = res[[x]]$artists[[1]]$name,
+                artist_uri = res[[x]]$artists[[1]]$id,
+                album_name = res[[x]]$album$name,
+                album_id = res[[x]]$album$id
             )
         })
-
+    } else {
+        tracks <- tibble()
     }
-    return(tibble(track_name = list(lapply(tracks, `[[`, 1)),
-                  track_uri = list(lapply(tracks, `[[`, 2))))
+    return(tracks)
 }
