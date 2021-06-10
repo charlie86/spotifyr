addc629ba406af7e06d89ffa2b5bf9b25178eddf
Daniel Antal <antaldaniel@users.noreply.github.com>
2021-06-10 17:01:35 +0200

    genius dependency

--- R/geniusR.R
+++ R/geniusR.R
@@ -16,8 +16,9 @@
 #'
 #' @export
 #' @importFrom tidyr nest unnest
-#' @importFrom purrr possibly
-#' @importFrom dplyr mutate select filter left_join
+#' @importFrom genius genius_album
+#' @importFrom purrr possibly map_df
+#' @importFrom dplyr mutate select filter left_join ungroup rename
 #' @importFrom tibble as_tibble
 
 
@@ -40,18 +41,26 @@ get_album_data <- function(artist,
         distinct(album_name) %>%
         mutate(artist = artist)
     # Create possible_album for potential error handling
-    possible_album <- possibly(genius_album, otherwise = as_tibble())
+    empty_album <- tibble (
+        track_n  = NA_real_,
+        line = NA_real_,
+        lyric = NA_character_,
+        track_title = NA_character_
+    )
+    possible_album <- possibly(genius::genius_album, otherwise = empty_album ) #as_tibble is not allowed
+    #PA <- possible_album(album_list$artist[1], album_list$album_name[1])
+
 
     album_lyrics <- map2(album_list$artist, album_list$album_name, function(x, y) possible_album(x, y) %>% mutate(album_name = y)) %>%
         map_df(function(x) nest(x, -c(track_title, track_n, album_name))) %>%
-        rename(lyrics = data) %>%
-        select(-track_title)
+        rename(lyrics = .data$data) %>%
+        select(-all_of("track_title"))
 
     # Acquire the lyrics for each track
     album_data <- artist_disco %>%
         left_join(album_lyrics, by = c('album_name', 'track_n'))
 
-    return(album_data)
+    album_data
 }
 
 #' Retrieve artist discography with song lyrics and audio info
--- cran-comments.md
+++ cran-comments.md
@@ -7,3 +7,5 @@
 ## To reviewer
 
 The package has been on CRAN for a very long time without changes, and eventually archived a short while ago. This is a long overdue minor release that fixes such issues without changing functionality.  This means fixing dozens of broken links, removing deprecated dependencies, and improving non-standard evaluation to current best practices. 
+
+Most examples are in \donttest{}.  This is a necessity, because the the entire package is working with the Spotify Web API, which requires personal authentication from the developers, and in case of interaction with third parties, authentication from the third parties, too. These functions were tested with devtools::run_examples() with the maintainers personal access token.
