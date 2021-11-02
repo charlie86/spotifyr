51e9fc32441f4656f26711db4add9785336f7f78
BradHill <bradhill119@gmail.com>
2021-11-02 16:06:36 -0500

    update playlists.R: - Check for nonencoded colon before attaching percent encoded prefix

--- R/playlists.R
+++ R/playlists.R
@@ -351,7 +351,7 @@ add_tracks_to_playlist <- function(playlist_id,
                                    authorization = get_spotify_authorization_code()
                                    ) {
 
-    uris <- ifelse ( ! grepl( "spotify%3Atrack%3A", uris),
+    uris <- ifelse ( ! grepl( "spotify%3Atrack%3A", gsub(":", "%3A", uris)),
              paste0("spotify%3Atrack%3A", uris),
              uris)
 
